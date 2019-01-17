6fe2fdf766c51d693f42832ca4f2a90e02c3b955
Mike Greer <mr.greer@gmail.com>
2019-01-17 08:01:43 +0000

    moving hexagon grid to component

---
+++ src/components/HexGrid.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import classNames from 'classnames';
+
+class Emitter extends React.Component {
+    render() {
+        const hexWidth = Math.sqrt(3) * this.props.hexScale;
+        return (
+            <g>
+                <circle cx={hexWidth/2} cy="0" r={this.props.hexScale/2} stroke="black" fill="none" strokeWidth="10"  />
+            </g>
+        );
+    }
+}
+
+class Router extends React.Component {
+    render() {
+        const hexWidth = Math.sqrt(3) * this.props.hexScale;
+        return (
+            <g>
+                <circle cx={hexWidth/2} cy="0" r={this.props.hexScale/2} stroke="red" fill="none" strokeWidth="5"  />
+            </g>
+        );
+    }
+}
+
+class Goal extends React.Component {
+    render() {
+        const hexWidth = Math.sqrt(3) * this.props.hexScale;
+        return (
+            <g>
+                <circle cx={hexWidth/2} cy="0" r={this.props.hexScale/2} stroke="blue" fill="none" strokeWidth="10"  />
+            </g>
+        );
+    }
+}
+
+class Ghost extends React.Component {
+    render() {
+        return (
+            <span className="ghostElement">
+                {this.props.currentlyAdding === 1 ? "emit":null}
+                {this.props.currentlyAdding === 2 ? "route":null}
+                {this.props.currentlyAdding === 3 ? "goal":null}
+            </span>
+        );
+    }
+}
+
+class Hexagon extends React.Component {
+    render () {
+        const scale = this.props.hexScale;
+        const width = Math.sqrt(3) * scale;
+        const hexPoints = [
+            {x: width, y: (scale/2)*-1},
+            {x: width, y: scale/2},
+            {x: width/2, y: scale},
+            {x: 0, y: scale/2},
+            {x: 0, y: (scale/2)*-1},
+            {x: width/2, y: scale*-1}
+        ];
+        let points = "";
+        hexPoints.forEach(point => {
+            points += point.x + "," + point.y + " ";
+        });
+        const center = this.props.coordinates;
+        return(
+            <g style = { 
+                {transform: "translate(" + center.x + "px, " + center.y + "px)"}
+            }>
+            <polygon
+                points={points}
+                stroke="#eeeeee" 
+                fill="none" 
+                strokeWidth="1"
+                className={classNames(
+                    { 'selected': this.props.selected },
+                    { 'hover': this.props.hover },
+                    "hex")}
+                style={
+                    {cursor: this.props.currentTool === 1 ? 'move':'default'}
+                }
+                onMouseDown={(cellIndex) => this.props.onMouseDown(this.props.cellIndex)}
+                // onMouseOver={(cellIndex) => this.props.onHover(this.props.cellIndex)}
+                onMouseUp={(cellIndex) => this.props.onMouseUp(this.props.cellIndex)}
+            >
+                {this.props.hover ? <Ghost currentlyAdding={this.props.currentlyAdding}></Ghost> : null}
+            ></polygon>
+            {this.props.cellType === 1 ? <Emitter hexScale={scale}></Emitter> : null}
+            {this.props.cellType === 2 ? <Router hexScale={scale}></Router> : null}
+            {this.props.cellType === 3 ? <Goal hexScale={scale}></Goal> : null}
+            </g>
+        );
+    }
+}
+
+class HexGrid extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hexScale: this.props.cellSize,
+        }
+    }
+
+    render() {
+        const hexGrid = [];
+        this.props.level.forEach((hex, index) => {
+            const cell = this.props.level[index];
+            hexGrid.push(<Hexagon
+                key={index}
+                cellIndex={index}
+                hexScale={this.state.hexScale}
+                coordinates = {{x: hex.gridX, y: hex.gridY}}
+                cellType={cell.type}
+                onMouseDown={(i) => this.props.onMouseDown(i)}
+                // onHover={(cellID) => this.props.onHover(cellID)}
+                onMouseUp={(i) => this.props.onMouseUp(i)}
+                selected={cell.selected}
+                // hover={cell.hover}
+                currentlyAdding={this.props.currentlyAdding}
+                currentTool={this.props.currentTool}
+            />);
+        });
+        const emitters = [];
+        this.props.emitters.forEach((emitter, index) => {
+            emitters.push(
+                <Emitter
+                    cellID={emitter.cellID}
+                    releaseFrequency={emitter.releaseFrequency}
+                    releaseRules={emitter.releaseRules}
+                ></Emitter>)
+        });
+
+        return(
+            <div className="grid">
+                <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" width="350" height="350">
+                    {hexGrid}
+                </svg>
+            </div>
+        );
+    }
+}
+
+export default HexGrid;
--- src/index.js
+++ src/index.js
@@ -7,151 +7,11 @@ import RuleEditor from './components/RuleEditor';
 import Toolbox from './components/Toolbox';
 import LLOutput from './components/LLOutput';
 import * as serviceWorker from './serviceWorker';
+import HexGrid from './components/HexGrid';
 // import ToolboxCreate from './components/ToolboxCreate';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
-
-class Emitter extends React.Component {
-    render() {
-        const hexWidth = Math.sqrt(3) * this.props.hexScale;
-        return (
-            <g>
-                <circle cx={hexWidth/2} cy="0" r={this.props.hexScale/2} stroke="black" fill="none" strokeWidth="10"  />
-            </g>
-        );
-    }
-}
-
-class Router extends React.Component {
-    render() {
-        const hexWidth = Math.sqrt(3) * this.props.hexScale;
-        return (
-            <g>
-                <circle cx={hexWidth/2} cy="0" r={this.props.hexScale/2} stroke="red" fill="none" strokeWidth="5"  />
-            </g>
-        );
-    }
-}
-
-class Goal extends React.Component {
-    render() {
-        const hexWidth = Math.sqrt(3) * this.props.hexScale;
-        return (
-            <g>
-                <circle cx={hexWidth/2} cy="0" r={this.props.hexScale/2} stroke="blue" fill="none" strokeWidth="10"  />
-            </g>
-        );
-    }
-}
-
-class Ghost extends React.Component {
-    render() {
-        return (
-            <span className="ghostElement">
-                {this.props.currentlyAdding === 1 ? "emit":null}
-                {this.props.currentlyAdding === 2 ? "route":null}
-                {this.props.currentlyAdding === 3 ? "goal":null}
-            </span>
-        );
-    }
-}
-
-class Hexagon extends React.Component {
-    render () {
-        const scale = this.props.hexScale;
-        const width = Math.sqrt(3) * scale;
-        const hexPoints = [
-            {x: width, y: (scale/2)*-1},
-            {x: width, y: scale/2},
-            {x: width/2, y: scale},
-            {x: 0, y: scale/2},
-            {x: 0, y: (scale/2)*-1},
-            {x: width/2, y: scale*-1}
-        ];
-
-        let points = "";
-        hexPoints.forEach(point => {
-            points += point.x + "," + point.y + " ";
-        });
-        
-        const center = this.props.coordinates;
-        return(
-            <g style = { 
-                {transform: "translate(" + center.x + "px, " + center.y + "px)"}
-            }>
-            <polygon
-                points={points}
-                stroke="#eeeeee" 
-                fill="none" 
-                strokeWidth="1"
-                className={classNames(
-                    { 'selected': this.props.selected },
-                    { 'hover': this.props.hover },
-                    "hex")}
-                style={
-                    {cursor: this.props.currentTool === 1 ? 'move':'default'}
-                }
-                onMouseDown={(cellIndex) => this.props.onMouseDown(this.props.cellIndex)}
-                // onMouseOver={(cellIndex) => this.props.onHover(this.props.cellIndex)}
-                onMouseUp={(cellIndex) => this.props.onMouseUp(this.props.cellIndex)}
-            >
-                {this.props.hover ? <Ghost currentlyAdding={this.props.currentlyAdding}></Ghost> : null}
-            ></polygon>
-            {this.props.cellType === 1 ? <Emitter hexScale={scale}></Emitter> : null}
-            {this.props.cellType === 2 ? <Router hexScale={scale}></Router> : null}
-            {this.props.cellType === 3 ? <Goal hexScale={scale}></Goal> : null}
-            </g>
-        );
-    }
-}
-
-class LogicGrid extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            hexScale: this.props.cellSize,
-        }
-    }
-
-    render() {
-        const hexGrid = [];
-        this.props.level.forEach((hex, index) => {
-            const cell = this.props.level[index];
-            hexGrid.push(<Hexagon
-                key={index}
-                cellIndex={index}
-                hexScale={this.state.hexScale}
-                coordinates = {{x: hex.gridX, y: hex.gridY}}
-                cellType={cell.type}
-                onMouseDown={(i) => this.props.onMouseDown(i)}
-                // onHover={(cellID) => this.props.onHover(cellID)}
-                onMouseUp={(i) => this.props.onMouseUp(i)}
-                selected={cell.selected}
-                // hover={cell.hover}
-                currentlyAdding={this.props.currentlyAdding}
-                currentTool={this.props.currentTool}
-            />);
-        });
-        const emitters = [];
-        this.props.emitters.forEach((emitter, index) => {
-            emitters.push(
-                <Emitter
-                    cellID={emitter.cellID}
-                    releaseFrequency={emitter.releaseFrequency}
-                    releaseRules={emitter.releaseRules}
-                ></Emitter>)
-        });
-
-        return(
-            <div className="grid">
-                <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" width="350" height="350">
-                    {hexGrid}
-                </svg>
-            </div>
-        );
-    }
-}
 
 class Game extends React.Component {
     constructor(props) {
@@ -448,7 +308,7 @@ class Game extends React.Component {
 
                 
                 <div className="level">
-                    <LogicGrid
+                    <HexGrid
                         level = {this.state.level}
                         emitters = {this.state.emitters}
                         onMouseDown = {(cellID) => this.cellClick(cellID)}
@@ -457,7 +317,7 @@ class Game extends React.Component {
                         currentlyAdding = {this.state.currentlyAdding}
                         currentTool = {this.state.currentTool}
                         cellSize = {this.state.cellSize}
-                    ></LogicGrid>
+                    ></HexGrid>
                     <LLOutput
                         level = {this.state.level}
                         width = {340}
