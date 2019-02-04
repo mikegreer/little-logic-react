fff2fae3c6fc0c8674a2d65614cd8319ca3e816e
Mike Greer <mr.greer@gmail.com>
2019-02-04 14:35:38 +0000

    direction arrow multi picker

--- src/components/RuleEditor.js
+++ src/components/RuleEditor.js
@@ -65,25 +65,53 @@ class TargetPicker extends React.Component {
     }
 }
 
-// class DirectionPickerValue extends React.Component {
-//     render() {
-//         return();
-//     }
-// }
-
-class DirectionPicker extends React.Component {
+class DirectionPickerValue extends React.Component {
     render() {
         const direction = (this.props.value * 60) - 60;
         let arrowStyle = {
             transform: `rotate(${direction}deg)`
         }
-        return (
-            <div 
-                className="direction-arrow-wrapper"
-                onClick={()=>this.props.onClick("direction", this.props.value + 1 > 5 ? 0 : this.props.value + 1)}
+        return(
+            <span 
+                className = {classNames({ 'selected': this.props.selected }, "direction-arrow")}
+                onClick={this.props.onClick}
             >
-                <img src="images/dir-picker-arrow.svg" style={arrowStyle} width="22"/>
-            </div>
+                <img src="images/dir-picker-arrow.svg" style={arrowStyle} width="28"/>
+            </span>
+        );
+    }
+}
+
+class DirectionPicker extends React.Component {
+    handleClick(value) {
+        this.props.onClick("direction", value);
+    }
+
+    render() {
+        const arrows = [];
+        for(let i = 0; i < 6; i++) {
+            arrows.push(
+                <DirectionPickerValue
+                    onClick = {(value) => this.handleClick(i)}
+                    value = {i}
+                    selected = {this.props.value === i}
+                />
+            )
+        }
+        const direction = (this.props.value * 60) - 60;
+        let arrowStyle = {
+            transform: `rotate(${direction}deg)`
+        }
+        return (
+            <span>
+                {/* <div 
+                    className="direction-arrow-wrapper"
+                    onClick={()=>this.props.onClick("direction", this.props.value + 1 > 5 ? 0 : this.props.value + 1)}
+                >
+                    <img src="images/dir-picker-arrow.svg" style={arrowStyle} width="22"/>
+                </div> */}
+                {arrows}
+            </span>
         );
     }
 }
